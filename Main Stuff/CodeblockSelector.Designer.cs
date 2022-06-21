66b4f8fb3415aa7f4df3ee7c7a62da100254a9ba
lily_ <lily_>
2022-06-21 17:07:41 -0300

    c

--- Main Stuff/CodeblockSelector.Designer.cs
+++ Main Stuff/CodeblockSelector.Designer.cs
@@ -99,17 +99,99 @@ namespace FNAF_Engine_Reborn.Main_Stuff
             System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("Office", new System.Windows.Forms.TreeNode[] {
             treeNode36,
             treeNode37});
-            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("Compare general values");
-            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("Set [] to []");
-            System.Windows.Forms.TreeNode treeNode41 = new System.Windows.Forms.TreeNode("Special Conditions", new System.Windows.Forms.TreeNode[] {
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("On cutscene [] start");
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("On cutscene [] loop");
+            System.Windows.Forms.TreeNode treeNode41 = new System.Windows.Forms.TreeNode("On cutscene [] break");
+            System.Windows.Forms.TreeNode treeNode42 = new System.Windows.Forms.TreeNode("On cutscene [] end");
+            System.Windows.Forms.TreeNode treeNode43 = new System.Windows.Forms.TreeNode("On minigame [] start");
+            System.Windows.Forms.TreeNode treeNode44 = new System.Windows.Forms.TreeNode("On minigame [] loop");
+            System.Windows.Forms.TreeNode treeNode45 = new System.Windows.Forms.TreeNode("On minigame [] break");
+            System.Windows.Forms.TreeNode treeNode46 = new System.Windows.Forms.TreeNode("On minigame [] end");
+            System.Windows.Forms.TreeNode treeNode47 = new System.Windows.Forms.TreeNode("Other", new System.Windows.Forms.TreeNode[] {
             treeNode39,
-            treeNode40});
-            System.Windows.Forms.TreeNode treeNode42 = new System.Windows.Forms.TreeNode("Engine");
-            System.Windows.Forms.TreeNode treeNode43 = new System.Windows.Forms.TreeNode("Sound");
-            System.Windows.Forms.TreeNode treeNode44 = new System.Windows.Forms.TreeNode("Game Control");
-            System.Windows.Forms.TreeNode treeNode45 = new System.Windows.Forms.TreeNode("Keyboard and Mouse");
-            System.Windows.Forms.TreeNode treeNode46 = new System.Windows.Forms.TreeNode("Player");
-            System.Windows.Forms.TreeNode treeNode47 = new System.Windows.Forms.TreeNode("Office");
+            treeNode40,
+            treeNode41,
+            treeNode42,
+            treeNode43,
+            treeNode44,
+            treeNode45,
+            treeNode46});
+            System.Windows.Forms.TreeNode treeNode48 = new System.Windows.Forms.TreeNode("Compare general values");
+            System.Windows.Forms.TreeNode treeNode49 = new System.Windows.Forms.TreeNode("Set [] to []");
+            System.Windows.Forms.TreeNode treeNode50 = new System.Windows.Forms.TreeNode("Special Conditions", new System.Windows.Forms.TreeNode[] {
+            treeNode48,
+            treeNode49});
+            System.Windows.Forms.TreeNode treeNode51 = new System.Windows.Forms.TreeNode("Engine");
+            System.Windows.Forms.TreeNode treeNode52 = new System.Windows.Forms.TreeNode("Sound");
+            System.Windows.Forms.TreeNode treeNode53 = new System.Windows.Forms.TreeNode("Game Control");
+            System.Windows.Forms.TreeNode treeNode54 = new System.Windows.Forms.TreeNode("Keyboard and Mouse");
+            System.Windows.Forms.TreeNode treeNode55 = new System.Windows.Forms.TreeNode("Player");
+            System.Windows.Forms.TreeNode treeNode56 = new System.Windows.Forms.TreeNode("If office state is []");
+            System.Windows.Forms.TreeNode treeNode57 = new System.Windows.Forms.TreeNode("If office is []");
+            System.Windows.Forms.TreeNode treeNode58 = new System.Windows.Forms.TreeNode("Change current office");
+            System.Windows.Forms.TreeNode treeNode59 = new System.Windows.Forms.TreeNode("Change current office state");
+            System.Windows.Forms.TreeNode treeNode60 = new System.Windows.Forms.TreeNode("Office", new System.Windows.Forms.TreeNode[] {
+            treeNode56,
+            treeNode57,
+            treeNode58,
+            treeNode59});
+            System.Windows.Forms.TreeNode treeNode61 = new System.Windows.Forms.TreeNode("Input");
+            System.Windows.Forms.TreeNode treeNode62 = new System.Windows.Forms.TreeNode("Set power usage to []");
+            System.Windows.Forms.TreeNode treeNode63 = new System.Windows.Forms.TreeNode("Set power level to []");
+            System.Windows.Forms.TreeNode treeNode64 = new System.Windows.Forms.TreeNode("If power is []%");
+            System.Windows.Forms.TreeNode treeNode65 = new System.Windows.Forms.TreeNode("If power is out");
+            System.Windows.Forms.TreeNode treeNode66 = new System.Windows.Forms.TreeNode("Remove []% of power");
+            System.Windows.Forms.TreeNode treeNode67 = new System.Windows.Forms.TreeNode("Add []% of power");
+            System.Windows.Forms.TreeNode treeNode68 = new System.Windows.Forms.TreeNode("Remove [] of power usage");
+            System.Windows.Forms.TreeNode treeNode69 = new System.Windows.Forms.TreeNode("Add [] of power usage");
+            System.Windows.Forms.TreeNode treeNode70 = new System.Windows.Forms.TreeNode("Power", new System.Windows.Forms.TreeNode[] {
+            treeNode62,
+            treeNode63,
+            treeNode64,
+            treeNode65,
+            treeNode66,
+            treeNode67,
+            treeNode68,
+            treeNode69});
+            System.Windows.Forms.TreeNode treeNode71 = new System.Windows.Forms.TreeNode("Office Objects");
+            System.Windows.Forms.TreeNode treeNode72 = new System.Windows.Forms.TreeNode("Camera Objects");
+            System.Windows.Forms.TreeNode treeNode73 = new System.Windows.Forms.TreeNode("Change animatronic [] state on office [] to []");
+            System.Windows.Forms.TreeNode treeNode74 = new System.Windows.Forms.TreeNode("Change animatronic [] state on camera [] to []");
+            System.Windows.Forms.TreeNode treeNode75 = new System.Windows.Forms.TreeNode("Reset animatronic [] state on camera []");
+            System.Windows.Forms.TreeNode treeNode76 = new System.Windows.Forms.TreeNode("Reset animatronic [] state on office");
+            System.Windows.Forms.TreeNode treeNode77 = new System.Windows.Forms.TreeNode("Animatronic [] state on office [] is [] ");
+            System.Windows.Forms.TreeNode treeNode78 = new System.Windows.Forms.TreeNode("Animatronic [] state on camera [] is [] ");
+            System.Windows.Forms.TreeNode treeNode79 = new System.Windows.Forms.TreeNode("Is animatronic [] on door []");
+            System.Windows.Forms.TreeNode treeNode80 = new System.Windows.Forms.TreeNode("Is animatronic [] on flashlight");
+            System.Windows.Forms.TreeNode treeNode81 = new System.Windows.Forms.TreeNode("Is animatronic [] on camera []");
+            System.Windows.Forms.TreeNode treeNode82 = new System.Windows.Forms.TreeNode("Set animatronic [] AI to []");
+            System.Windows.Forms.TreeNode treeNode83 = new System.Windows.Forms.TreeNode("Move animatronic [] to office");
+            System.Windows.Forms.TreeNode treeNode84 = new System.Windows.Forms.TreeNode("Move animatronic [] to door []");
+            System.Windows.Forms.TreeNode treeNode85 = new System.Windows.Forms.TreeNode("Move animatronic [] to flashlight");
+            System.Windows.Forms.TreeNode treeNode86 = new System.Windows.Forms.TreeNode("Move animatronic [] to camera []");
+            System.Windows.Forms.TreeNode treeNode87 = new System.Windows.Forms.TreeNode("Animatronic", new System.Windows.Forms.TreeNode[] {
+            treeNode73,
+            treeNode74,
+            treeNode75,
+            treeNode76,
+            treeNode77,
+            treeNode78,
+            treeNode79,
+            treeNode80,
+            treeNode81,
+            treeNode82,
+            treeNode83,
+            treeNode84,
+            treeNode85,
+            treeNode86});
+            System.Windows.Forms.TreeNode treeNode88 = new System.Windows.Forms.TreeNode("Blue screen with title [] and text [] with font size []");
+            System.Windows.Forms.TreeNode treeNode89 = new System.Windows.Forms.TreeNode("Call function [] with argument []");
+            System.Windows.Forms.TreeNode treeNode90 = new System.Windows.Forms.TreeNode("Other", new System.Windows.Forms.TreeNode[] {
+            treeNode88,
+            treeNode89});
+            System.Windows.Forms.TreeNode treeNode91 = new System.Windows.Forms.TreeNode("If argument is []");
+            System.Windows.Forms.TreeNode treeNode92 = new System.Windows.Forms.TreeNode("Function Exclusives", new System.Windows.Forms.TreeNode[] {
+            treeNode91});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CodeblockSelector));
             this.Events = new System.Windows.Forms.TreeView();
             this.Actions = new System.Windows.Forms.TreeView();
@@ -200,13 +282,32 @@ namespace FNAF_Engine_Reborn.Main_Stuff
             treeNode37.Text = "Door [] is []";
             treeNode38.Name = "Office";
             treeNode38.Text = "Office";
+            treeNode39.Name = "On cutscene [] start";
+            treeNode39.Text = "On cutscene [] start";
+            treeNode40.Name = "On cutscene [] loop";
+            treeNode40.Text = "On cutscene [] loop";
+            treeNode41.Name = "On cutscene [] break";
+            treeNode41.Text = "On cutscene [] break";
+            treeNode42.Name = "On cutscene [] end";
+            treeNode42.Text = "On cutscene [] end";
+            treeNode43.Name = "On minigame [] start";
+            treeNode43.Text = "On minigame [] start";
+            treeNode44.Name = "On minigame [] loop";
+            treeNode44.Text = "On minigame [] loop";
+            treeNode45.Name = "On minigame [] break";
+            treeNode45.Text = "On minigame [] break";
+            treeNode46.Name = "On minigame [] end";
+            treeNode46.Text = "On minigame [] end";
+            treeNode47.Name = "Other";
+            treeNode47.Text = "Other";
             this.Events.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode17,
             treeNode21,
             treeNode27,
             treeNode32,
             treeNode35,
-            treeNode38});
+            treeNode38,
+            treeNode47});
             this.Events.Size = new System.Drawing.Size(461, 477);
             this.Events.TabIndex = 42;
             // 
@@ -219,32 +320,112 @@ namespace FNAF_Engine_Reborn.Main_Stuff
             this.Actions.LineColor = System.Drawing.Color.Red;
             this.Actions.Location = new System.Drawing.Point(12, 10);
             this.Actions.Name = "Actions";
-            treeNode39.Name = "Compare general values";
-            treeNode39.Text = "Compare general values";
-            treeNode40.Name = "Set [] to []";
-            treeNode40.Text = "Set [] to []";
-            treeNode41.Name = "Special Conditions";
-            treeNode41.Text = "Special Conditions";
-            treeNode42.Name = "Engine";
-            treeNode42.Text = "Engine";
-            treeNode43.Name = "Sound";
-            treeNode43.Text = "Sound";
-            treeNode44.Name = "Game Control";
-            treeNode44.Text = "Game Control";
-            treeNode45.Name = "Keyboard and Mouse";
-            treeNode45.Text = "Keyboard and Mouse";
-            treeNode46.Name = "Player";
-            treeNode46.Text = "Player";
-            treeNode47.Name = "Office";
-            treeNode47.Text = "Office";
+            treeNode48.Name = "Compare general values";
+            treeNode48.Text = "Compare general values";
+            treeNode49.Name = "Set [] to []";
+            treeNode49.Text = "Set [] to []";
+            treeNode50.Name = "Special Conditions";
+            treeNode50.Text = "Special Conditions";
+            treeNode51.Name = "Engine";
+            treeNode51.Text = "Engine";
+            treeNode52.Name = "Sound";
+            treeNode52.Text = "Sound";
+            treeNode53.Name = "Game Control";
+            treeNode53.Text = "Game Control";
+            treeNode54.Name = "Keyboard and Mouse";
+            treeNode54.Text = "Keyboard and Mouse";
+            treeNode55.Name = "Player";
+            treeNode55.Text = "Player";
+            treeNode56.Name = "If office state is []";
+            treeNode56.Text = "If office state is []";
+            treeNode57.Name = "If office is []";
+            treeNode57.Text = "If office is []";
+            treeNode58.Name = "Change current office";
+            treeNode58.Text = "Change current office";
+            treeNode59.Name = "Change current office state";
+            treeNode59.Text = "Change current office state";
+            treeNode60.Name = "Office";
+            treeNode60.Text = "Office";
+            treeNode61.Name = "Input";
+            treeNode61.Text = "Input";
+            treeNode62.Name = "Set power usage to []";
+            treeNode62.Text = "Set power usage to []";
+            treeNode63.Name = "Set power level to []";
+            treeNode63.Text = "Set power level to []";
+            treeNode64.Name = "If power is []%";
+            treeNode64.Text = "If power is []%";
+            treeNode65.Name = "If power is out";
+            treeNode65.Text = "If power is out";
+            treeNode66.Name = "Remove []% of power";
+            treeNode66.Text = "Remove []% of power";
+            treeNode67.Name = "Add []% of power";
+            treeNode67.Text = "Add []% of power";
+            treeNode68.Name = "Remove [] of power usage";
+            treeNode68.Text = "Remove [] of power usage";
+            treeNode69.Name = "Add [] of power usage";
+            treeNode69.Text = "Add [] of power usage";
+            treeNode70.Name = "Power";
+            treeNode70.Text = "Power";
+            treeNode71.Name = "Office Objects";
+            treeNode71.Text = "Office Objects";
+            treeNode72.Name = "Camera Objects";
+            treeNode72.Text = "Camera Objects";
+            treeNode73.Name = "Change animatronic [] state on office [] to []";
+            treeNode73.Text = "Change animatronic [] state on office [] to []";
+            treeNode74.Name = "Change animatronic [] state on camera [] to []";
+            treeNode74.Text = "Change animatronic [] state on camera [] to []";
+            treeNode75.Name = "Reset animatronic [] state on camera []";
+            treeNode75.Text = "Reset animatronic [] state on camera []";
+            treeNode76.Name = "Reset animatronic [] state on office";
+            treeNode76.Text = "Reset animatronic [] state on office";
+            treeNode77.Name = "Animatronic [] state on office [] is [] ";
+            treeNode77.Text = "Animatronic [] state on office [] is [] ";
+            treeNode78.Name = "Animatronic [] state on camera [] is [] ";
+            treeNode78.Text = "Animatronic [] state on camera [] is [] ";
+            treeNode79.Name = "Is animatronic [] on door []";
+            treeNode79.Text = "Is animatronic [] on door []";
+            treeNode80.Name = "Is animatronic [] on flashlight";
+            treeNode80.Text = "Is animatronic [] on flashlight";
+            treeNode81.Name = "Is animatronic [] on camera []";
+            treeNode81.Text = "Is animatronic [] on camera []";
+            treeNode82.Name = "Set animatronic [] AI to []";
+            treeNode82.Text = "Set animatronic [] AI to []";
+            treeNode83.Name = "Move animatronic [] to office";
+            treeNode83.Text = "Move animatronic [] to office";
+            treeNode84.Name = "Move animatronic [] to door []";
+            treeNode84.Text = "Move animatronic [] to door []";
+            treeNode85.Name = "Move animatronic [] to flashlight";
+            treeNode85.NodeFont = new System.Drawing.Font("Segoe UI", 9F);
+            treeNode85.Text = "Move animatronic [] to flashlight";
+            treeNode86.Name = "Move animatronic [] to camera []";
+            treeNode86.Text = "Move animatronic [] to camera []";
+            treeNode87.Name = "Animatronic";
+            treeNode87.Text = "Animatronic";
+            treeNode88.Name = "Blue screen with title [] and text [] with font size []";
+            treeNode88.Text = "Blue screen with title [] and text [] with font size []";
+            treeNode89.Name = "Call function [] with argument []";
+            treeNode89.Text = "Call function [] with argument []";
+            treeNode90.Name = "Other";
+            treeNode90.Text = "Other";
+            treeNode91.Name = "If argument is []";
+            treeNode91.Text = "If argument is []";
+            treeNode92.Name = "Function Exclusives";
+            treeNode92.Text = "Function Exclusives";
             this.Actions.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode41,
-            treeNode42,
-            treeNode43,
-            treeNode44,
-            treeNode45,
-            treeNode46,
-            treeNode47});
+            treeNode50,
+            treeNode51,
+            treeNode52,
+            treeNode53,
+            treeNode54,
+            treeNode55,
+            treeNode60,
+            treeNode61,
+            treeNode70,
+            treeNode71,
+            treeNode72,
+            treeNode87,
+            treeNode90,
+            treeNode92});
             this.Actions.Size = new System.Drawing.Size(461, 477);
             this.Actions.TabIndex = 43;
             // 
@@ -254,8 +435,8 @@ namespace FNAF_Engine_Reborn.Main_Stuff
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.ClientSize = new System.Drawing.Size(485, 501);
-            this.Controls.Add(this.Actions);
             this.Controls.Add(this.Events);
+            this.Controls.Add(this.Actions);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.ForeColor = System.Drawing.SystemColors.Control;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -268,7 +449,7 @@ namespace FNAF_Engine_Reborn.Main_Stuff
 
         #endregion
 
-        private System.Windows.Forms.TreeView Events;
+        private new System.Windows.Forms.TreeView Events;
         private System.Windows.Forms.TreeView Actions;
     }
 }
